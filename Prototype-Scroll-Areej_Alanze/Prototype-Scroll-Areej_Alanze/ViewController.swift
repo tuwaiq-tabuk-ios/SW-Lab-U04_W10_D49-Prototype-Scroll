c2df7b90758faeac0cd559ecc04de21dbe8302e6
A A <aa@Areej.local>
2021-12-15 20:47:42 +0300

    Prototype-Scroll

---
+++ Prototype-Scroll-Areej_Alanze/Prototype-Scroll-Areej_Alanze/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Prototype-Scroll-Areej_Alanze
+//
+//  Created by A A on 15/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureLabel()
+  }
+  
+  
+  func configureLabel() {
+    let description = """
+  
+  
+  ★★1★★
+  
+  Use frameLayoutGuide when you want to create Auto Layout constraints that explicitly involve the frame rectangle of the scroll view itself, as opposed to its content rectangle.
+  Use contentLayoutGuide when you want to create Auto Layout constraints related to the content area of a scroll view.
+  
+  
+  ★★2★★
+  
+  Use frameLayoutGuide when you want to create Auto Layout constraints that explicitly involve the frame rectangle of the scroll view itself, as opposed to its content rectangle.
+  Use contentLayoutGuide when you want to create Auto Layout constraints related to the content area of a scroll view.
+  
+  
+  ★★3★★
+  
+  Use frameLayoutGuide when you want to create Auto Layout constraints that explicitly involve the frame rectangle of the scroll view itself, as opposed to its content rectangle.
+  Use contentLayoutGuide when you want to create Auto Layout constraints related to the content area of a scroll view.
+  
+  
+  ★★4★★
+  
+  Use frameLayoutGuide when you want to create Auto Layout constraints that explicitly involve the frame rectangle of the scroll view itself, as opposed to its content rectangle.
+  Use contentLayoutGuide when you want to create Auto Layout constraints related to the content area of a scroll view.
+  
+  
+  """
+    descriptionLabel.text = description
+    
+  }
+  
+}
